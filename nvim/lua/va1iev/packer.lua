9c867435ba1a10368e4818e7446e29caffc7ef6a
Sergey Valiev <svaliev@gmail.com>
2023-02-14 23:32:54 +0300

    colors

--- nvim/lua/va1iev/packer.lua
+++ nvim/lua/va1iev/packer.lua
@@ -57,6 +57,8 @@ return require('packer').startup(function(use)
 
   use('savq/melange-nvim')
   use("EdenEast/nightfox.nvim")
+  use('folke/tokyonight.nvim')
+
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
     requires = { {'nvim-lua/plenary.nvim'} }

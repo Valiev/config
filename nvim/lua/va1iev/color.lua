88bf34b23b575448739030016b49bfbacf68bcde
Sergey Valiev <svaliev@gmail.com>
2023-02-14 23:31:40 +0300

    vim: colors

--- nvim/lua/va1iev/color.lua
+++ nvim/lua/va1iev/color.lua
@@ -3,7 +3,12 @@
 
 vim.opt.background = "light"
 -- local colorscheme = "ayu-light"
-local colorscheme = "ayu-mirage"
+-- local colorscheme = "ayu-mirage"
+-- local colorscheme = "ayu-mirage"
+-- local colorscheme = "tokyonight-day"
+-- local colorscheme = "tokyonight-night"
+local colorscheme = "tokyonight-storm"
+-- local colorscheme = "tokyonight-moon"
 vim.cmd.colorscheme(colorscheme)
 -- require('ayu').setup({
 --   mirage = false

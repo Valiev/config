38fb75f4c6f9885c4460b16c83020c9dc89d6572
Sergey Valiev <svaliev@gmail.com>
2023-05-01 20:15:35 +0300

    nvim: colors

--- nvim/lua/va1iev/color.lua
+++ nvim/lua/va1iev/color.lua
@@ -1,16 +1,21 @@
 -- local colorscheme = "nordfox"
 -- local colorscheme = "oxocarbon"
 
+vim.g.solarized_italic_comments = true
+vim.g.solarized_contrast = true
+require('solarized').set()
+
+vim.g.solarized_termcolors = 256
 vim.opt.background = "light"
+-- vim.opt.background = "dark"
 -- local colorscheme = "ayu-light"
 -- local colorscheme = "ayu-mirage"
--- local colorscheme = "ayu-mirage"
+-- local colorscheme = "solarized"
 -- local colorscheme = "tokyonight-day"
 -- local colorscheme = "tokyonight-night"
-local colorscheme = "tokyonight-storm"
+-- local colorscheme = "tokyonight-storm"
 -- local colorscheme = "tokyonight-moon"
-vim.cmd.colorscheme(colorscheme)
+-- vim.cmd.colorscheme(colorscheme)
 -- require('ayu').setup({
 --   mirage = false
---
 -- })

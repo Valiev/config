aafe980c1b75aedb09e79d173ebe6169c8df674c
Sergey Valiev <svaliev@joom.com>
2024-10-20 11:28:23 +0300

    vim: fixes

--- nvim/.config/nvim/lua/va1iev/packer.lua
+++ nvim/.config/nvim/lua/va1iev/packer.lua
@@ -45,8 +45,8 @@ return require('packer').startup(function(use)
     'nvim-tree/nvim-tree.lua',
     requires = {
       'nvim-tree/nvim-web-devicons', -- optional, for file icons
-    },
-    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+    }
+    -- tag = 'nightly' -- optional, updated every week. (see issue #1193)
   })
   use({
     "folke/trouble.nvim",

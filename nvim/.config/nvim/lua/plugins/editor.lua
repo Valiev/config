ed77438a88b04dc5f8fd9fa624100ffb28d636bc
Sergey Valiev <svaliev@joom.com>
2024-12-28 11:41:40 +0300

    vim/editor: cosmetic fix

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -1,27 +1,19 @@
 return {
   {
-    "kylechui/nvim-surround",
+    'kylechui/nvim-surround',
     config = function()
-      require("nvim-surround").setup({})
+      require('nvim-surround').setup({})
       -- todo: check integration with
       -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end
   },
   {'ntpeters/vim-better-whitespace'},
   {'rmagatti/alternate-toggler'},
-  {"powerman/vim-plugin-ruscmd"},
-  -- {
-  --   'Wansmer/langmapper.nvim',
-  --   lazy = false,
-  --   priority = 1, -- High priority is needed if you will use `autoremap()`
-  --   config = function()
-  --     require('langmapper').setup({--[[ your config ]]})
-  --   end,
-  -- },
+  {'powerman/vim-plugin-ruscmd'},
   {
     'windwp/nvim-autopairs',
     config = function()
-      require("nvim-autopairs").setup({
+      require('nvim-autopairs').setup({
         check_ts = true,
       })
     end
@@ -39,26 +31,26 @@ return {
   {'junegunn/vim-easy-align'},
   {'junegunn/vim-after-object'},
   {
-    "folke/todo-comments.nvim",
-    dependencies = {"nvim-lua/plenary.nvim"},
+    'folke/todo-comments.nvim',
+    dependencies = {'nvim-lua/plenary.nvim'},
     -- TODO
     -- HACK
   },
 
-    -- movement
+  -- movement
 
-    {
-      "phaazon/hop.nvim",
-      config = function()
-        require('hop').setup({
-          keys = "qweroiuasdflkjghvn"
-        })
-      end
-    },
-    {
-      "karb94/neoscroll.nvim",
-      config = function ()
-        require('neoscroll').setup({})
-      end
-    }
+  {
+    'phaazon/hop.nvim',
+    config = function()
+      require('hop').setup({
+        keys = 'qweroiuasdflkjghvn'
+      })
+    end
+  },
+  {
+    'karb94/neoscroll.nvim',
+    config = function ()
+      require('neoscroll').setup({})
+    end
   }
+}

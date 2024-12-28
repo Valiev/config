a1b667d843c9bc819d946bb8c3273b8091ac46dc
Sergey Valiev <svaliev@joom.com>
2024-12-28 13:05:48 +0300

    vim/editor: fzf-lua plugin

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -52,5 +52,14 @@ return {
     config = function ()
       require('neoscroll').setup({})
     end
-  }
+  },
+  {
+    'ibhagwan/fzf-lua',
+    -- optional for icon support
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      -- calling `setup` is optional for customization
+      require('fzf-lua').setup({})
+    end
+  },
 }

514c06a97b1e87199ad0ac3cc4662adc2b14c0ea
Sergey Valiev <svaliev@joom.com>
2025-01-15 13:15:07 +0300

    vim/helper: format

--- nvim/.config/nvim/lua/helpers/func.lua
+++ nvim/.config/nvim/lua/helpers/func.lua
@@ -1,28 +1,28 @@
 local Func = {}
 
 function Func.map(mode, lhs, rhs, opts)
-    local options = { noremap = true }
-    opts = opts or {}
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  local options = { noremap = true }
+  opts = opts or {}
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 function Func.nmap(lhs, rhs, opts)
-    return Func.map("n", lhs, rhs, opts)
+  return Func.map("n", lhs, rhs, opts)
 end
 
 function Func.imap(lhs, rhs, opts)
-    return Func.map("i", lhs, rhs, opts)
+  return Func.map("i", lhs, rhs, opts)
 end
 
 function Func.vmap(lhs, rhs, opts)
-    return Func.map("v", lhs, rhs, opts)
+  return Func.map("v", lhs, rhs, opts)
 end
 
 function Func.xmap(lhs, rhs, opts)
-    return Func.map("x", lhs, rhs, opts)
+  return Func.map("x", lhs, rhs, opts)
 end
 
 return Func
